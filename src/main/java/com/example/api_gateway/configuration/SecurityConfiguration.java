fd01b38117ba01f30416d7b17740ddbcdc7a33d5
Dasha <dashavavilova2002@icloud.com>
2025-05-01 17:12:57 +0300

    add api gateway for geospatial

---
+++ src/main/java/com/example/api_gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.api_gateway.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .authorizeExchange(
+                        request -> request
+                                .pathMatchers("/error").permitAll()
+                                .anyExchange().permitAll()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+}
