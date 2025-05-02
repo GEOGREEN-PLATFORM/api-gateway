1583a4b1895fa4177fe8431511adb1c43536e349
Dasha <dashavavilova2002@icloud.com>
2025-05-02 10:32:03 +0300

    add all microservices to one swagger

--- src/main/java/com/example/api_gateway/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/api_gateway/configuration/SecurityConfiguration.java
@@ -1,20 +1,12 @@
 package com.example.api_gateway.configuration;
 
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springdoc.core.customizers.OpenApiCustomizer;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -34,29 +26,5 @@ public class SecurityConfiguration {
                         .jwt(Customizer.withDefaults())
                 );
         return http.build();
-    }
-
-    @Bean
-    public GroupedOpenApi service1Api() {
-        return GroupedOpenApi.builder()
-                .group("service-1")
-                .pathsToMatch("/service-1/**")
-                .addOpenApiCustomizer(serverUrlCustomizer("/service-1"))
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi service2Api() {
-        return GroupedOpenApi.builder()
-                .group("service-2")
-                .pathsToMatch("/service-2/**")
-                .addOpenApiCustomizer(serverUrlCustomizer("/service-2"))
-                .build();
-    }
-
-    private OpenApiCustomizer serverUrlCustomizer(String prefix) {
-        return openApi -> {
-            openApi.setServers(List.of(new Server().url(prefix)));
-        };
     }
 }
